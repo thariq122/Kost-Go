869be03320c90777f0ec8ac9d9d76d5327393e49
Fairuz06 <rafifairuz@upi.edu>
2026-06-02 11:06:37 +0700

    Aku masuk

--- lib/main.dart
+++ lib/main.dart
@@ -25,7 +25,7 @@ class MyApp extends StatelessWidget {
   Widget build(BuildContext context) {
     return MaterialApp(
       debugShowCheckedModeBanner: false,
-      title: 'KostGo Bro',
+      title: 'KostGo',
       theme: ThemeData(
         brightness: Brightness.dark,
         textTheme: kTextTheme,
